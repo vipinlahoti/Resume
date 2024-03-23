4f2b6da96054414daa5352f0a0143c089ae8d73e
Vipin L <vipinlahoti27@gmail.com>
2024-03-23 13:31:13 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       <Components.Hero
         title="I'm Vipin Lahoti"
         description="Frontend developer specializing in React.js, passionate about creating engaging and user-friendly web interfaces."
-        image="/2.jpeg"
       >
         <Components.Features />
         <Components.Profiles />
